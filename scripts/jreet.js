3fb6b24b2398c4b380226a3c3121c522f4fdf073
Rusted <129854755+whitetshirte@users.noreply.github.com>
2023-10-08 12:39:01 +0530

    Update jreet.js

--- scripts/jreet.js
+++ scripts/jreet.js
@@ -4,7 +4,7 @@ let hour = d.getHours();
 if (hour < 12)
 {
 	document.getElementById("greeting").innerHTML = "GOOD MORNING";
-}else if (hour > 12 && hour < 18) {
+}else if (hour >= 12 && hour < 18) {
 	document.getElementById("greeting").innerHTML = "GOOD AFTERNOON";
 }else{
 	document.getElementById("greeting").innerHTML = "GOOD EVENING";
