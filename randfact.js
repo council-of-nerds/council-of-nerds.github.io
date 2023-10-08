bea386857d6f1ffc2e9472eca4e4b70c552b9865
Asif C.A <111337174+asif-c-a@users.noreply.github.com>
2023-10-08 10:44:01 +0530

    Add files via upload

---
+++ randfact.js
@@ -0,0 +1,28 @@
+
+var fact = Math.floor(Math.random() * factlist.length);
+
+if (fact == 0) {
+	document.getElementById("fact").innerHTML = "Increased levels of carbon dioxide (CO2) in the atmosphere can lead to ocean acidification. This makes it difficult for organisms like corals, shellfish, and some fish to build and maintain their calcium carbonate shells or skeletons.";
+}else if(fact==1) {
+	document.getElementById("fact").innerHTML = "pH Level:- Ideal Range: 7.2 to 7.8 pH measures the acidity or alkalinity of the water. A pH level within this range is comfortable for swimmers and helps maintain water quality";
+}else if(fact==2) {
+	document.getElementById("fact").innerHTML = "Water Temperature:- Ideal Range: 78°F to 84°F (25°C to 29°C) Most people find water within this temperature range to be comfortable for swimming. However, personal preferences can vary.";
+}else if(fact==3) {
+	document.getElementById("fact").innerHTML = "Dissolved Oxygen (DO) Levels:- Ideal Range: 5 to 8 mg/L (milligrams per liter) Dissolved oxygen is vital for aquatic life and indicates the water's ability to support fish and other organisms. Levels in this range ensure sufficient oxygen for swimmers and aquatic species.";
+}else if(fact==4) {
+	document.getElementById("fact").innerHTML = "Coliform Levels (Escherichia coli, or E. coli):- Safe Level: Generally, the presence of E. coli should be below 2000 MPNper 100 milliliters of water for safe swimming.How ever a number of 125MPN/100ml is very safe for aquatic activities. E. coli is an indicator of fecal contamination. Elevated levels can indicate a risk of waterborne diseases. Swimming in water with high coliform levels can be unsafe.";
+}else if(fact==5) {
+	document.getElementById("fact").innerHTML = "Sunscreen and cosmetics can have various effects on swimming water when people bathe in pools or natural bodies of water like lakes or oceans.";"Chemical Discharge: Sunscreen and cosmetics often contain chemicals, including UV filters, fragrances, and preservatives. When people swim wearing these products, some of these chemicals can wash off into the water. In the case of sunscreen, this can help protect the skin from harmful UV radiation, but it can also introduce these chemicals into the aquatic environment.";
+}else if(fact==6) {
+	document.getElementById("fact").innerHTML = "Chemical Discharge: Sunscreen and cosmetics often contain chemicals, including UV filters, fragrances, and preservatives. When people swim wearing these products, some of these chemicals can wash off into the water. In the case of sunscreen, this can help protect the skin from harmful UV radiation, but it can also introduce these chemicals into the aquatic environment.";
+}else if(fact==7) {
+	document.getElementById("fact").innerHTML = "Environmental Impact: The discharge of sunscreen and cosmetics into natural bodies of water can potentially have environmental impacts. Some chemicals in these products may be harmful to aquatic life, especially in high concentrations. For instance, certain ingredients in sunscreen have been associated with coral bleaching when released into coral reef areas.";
+}else if(fact==8) {
+	document.getElementById("fact").innerHTML ="Biodegradable Products: Some sunscreen and cosmetic brands offer biodegradable or reef-safe products designed to minimize their environmental impact. These products typically contain ingredients that are less harmful to aquatic life and are less likely to contribute to coral reef degradation.";
+}else if(fact==9) {
+	document.getElementById("fact").innerHTML = "Migration Marvels: Aquatic species like salmon and sea turtles undertake incredible long-distance migrations, navigating vast ocean distances to return to their birthplaces to reproduce.";
+}else if(fact==10) {
+	document.getElementById("fact").innerHTML = "Freshwater vs. Saltwater: Aquatic species are adapted to either freshwater or saltwater environments. For example, saltwater fish have specialized mechanisms to maintain salt balance, while freshwater fish do the opposite.";
+}else if(fact==11) {
+	document.getElementById("fact").innerHTML = "Coral Symbiosis: Coral reefs are formed by tiny coral polyps that have a mutualistic relationship with algae. The algae provide the corals with food through photosynthesis, while the corals provide shelter and nutrients to the algae.";
+}
